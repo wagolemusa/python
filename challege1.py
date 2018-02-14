5f1b7f619bb6e28ff2cb01886df659fa814b79a4
kisumutalent <homiemusa@gmail.com>
2018-02-14 09:17:04 +0300

    Challege uppercase

---
+++ challege1.py
@@ -0,0 +1,11 @@
+def lowercase(string):
+     return string.lower()
+
+def uppercase(latter):
+     return latter.upper()
+
+
+print uppercase("refuge")
+print uppercase("wise")
+print lowercase("ARG")
+print lowercase("TRINKRT")
