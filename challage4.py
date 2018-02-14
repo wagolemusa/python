1acca23966c9a1faaa704434a187c79c675e3ced
kisumutalent <homiemusa@gmail.com>
2018-02-14 10:29:37 +0300

    Reverse slicncing

---
+++ challage4.py
@@ -0,0 +1,15 @@
+#make a function reverse_struing that is gaven a string
+#return that string in reverse
+
+#
+
+#>>>>>>>>reverse_string("arg")
+
+#gra
+#>>>>>>>reverse_string("Hi!")
+
+def reverse_string(string):
+     return string[::-1]
+
+print reverse_string("refuge")
+print reverse_string("HI!")
